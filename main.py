60c60c6b4437efbbb93bcfdf8090434e725b6c53
Чащихин Сергей <Inko-75@yandex.ru>
2023-02-12 01:20:38 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Функция расчета урона при атаке."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -19,6 +20,9 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Функция тренировки защиты персонажа.
+    Расчет количества заблокированного урона.
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -28,6 +32,9 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция тренировки специальных навыков персонажа.
+    Возвращает количество единиц навыка.
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -38,6 +45,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция выбора вида тренировки."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -62,6 +70,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция выбора типа персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
